7c843f21f68b209065fa82e1560f84d19d8b766d
Vik Boyechko <vik.boyechko@gmail.com>
2023-11-11 13:31:40 -0600

    Generator is working, but prompt replies aren't being considered yet

--- script.js
+++ script.js
@@ -16,36 +16,60 @@ generateBtn.addEventListener("click", writePassword);
 // My Code
 
 function generatePassword() {
-  var lettersLower = "abcdefghijklmnopqrstuvwxyz";
-  var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-  var randomNum = Math.floor(Math.random() * 10);
 
-  var password = "";
+  var lettersLower = "abcdefghijklmnopqrstuvwxyz"; // using a string rather than array to make the code easier to read
+  var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // same as above
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // excluded the double-quotation mark so it doesn't break the string
+  var randomNum = Math.floor(Math.random() * 10); // random number from 0 to 9
 
+  var password = ""; // initialize the var with an empty string
+
+  // let's declare  a function that can loop if the passLength is out of bounds, before continuing to the next prompt
+
+  var passLength; // first declares the passLength variable, so we can refer to it inside the loop
+  
   function passLengthValidation() {
-    var passLength = parseInt(prompt("How long do you want the password to be? "));
+    passLength = parseInt(prompt("How long do you want the password to be? "));
       if ((passLength < 8) || (passLength > 128)) {
         alert("Password must be between 8 and 128 characters");
-        passLengthValidation();
-    }
+        passLengthValidation(); // runs the function again, although maybe there is an alternative like 'continue' or 'return'
+    return passLength; // saves the passLength variable to use globally
+      }
   }
-  passLengthValidation();
+  passLengthValidation(); // calls the function above
 
+  // using the confirm function only allows OK or Cancel. In the future I want to use another function that can customize the button text
   var haveLettersLower = confirm("Do you want to include lowercase letters?");
   var haveLettersUpper = confirm("Do you want to include uppercase letters?");
   var haveRandomNums = confirm("Do you want numbers?")
   var haveSpecialChars = confirm("Do you want to include special characters?")
 
-
+  // can either use a for or while loop, but in this case 'for' works
   for (var i = 0; i < passLength; i++) {
-    var charType = Math.floor(Math.random() * 4); // For each password character, choose case 0, 1, 2, or 3
-    
-    var randomChar = Math.floor(Math.random() * 10);
-    password += randomChar;
-    i++;
-  }
-  console.log(password); 
 
+    // For each password character, choose to use case 0, 1, 2, or 3, which represents lowercase, uppercase, number, or special character
+    var charType = Math.floor(Math.random() * 4); 
+    
+    // the switch is automatically choosing a case during each iteration of the loop
+    switch (charType) {
+
+      case 0:
+        password += lettersLower.charAt(Math.floor(Math.random() * lettersLower.length)); // selects a random lowercase letter
+        break; // stops the current 'switch' choice and continues the 'for' loop
+
+      case 1:
+        password += lettersUpper.charAt(Math.floor(Math.random() * lettersUpper.length)); // selects a random uppercase letter
+        break;
+
+      case 2:
+        password += Math.floor(Math.random() * 10); // selects a random number 0-9
+        break;
+
+      case 3:
+        password += specialChars.charAt(Math.floor(Math.random() * specialChars.length)); // selects a random special character
+        break;
+    }
+  }
+  console.log(password);
   return password;
 }
