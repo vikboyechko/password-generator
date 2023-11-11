fa932ae4c4bc6b184d6e22d10f80e31e9303a138
Vik Boyechko <vik.boyechko@gmail.com>
2023-11-11 13:03:24 -0600

    set the stage for inputs, now need to add cases

--- script.js
+++ script.js
@@ -16,11 +16,31 @@ generateBtn.addEventListener("click", writePassword);
 // My Code
 
 function generatePassword() {
-  var passLength = parseInt(prompt("What length do you want"))
-  var password = "";
-  var i = 0;
+  var lettersLower = "abcdefghijklmnopqrstuvwxyz";
+  var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  var randomNum = Math.floor(Math.random() * 10);
 
-  while (i < passLength) {
+  var password = "";
+
+  function passLengthValidation() {
+    var passLength = parseInt(prompt("How long do you want the password to be? "));
+      if ((passLength < 8) || (passLength > 128)) {
+        alert("Password must be between 8 and 128 characters");
+        passLengthValidation();
+    }
+  }
+  passLengthValidation();
+
+  var haveLettersLower = confirm("Do you want to include lowercase letters?");
+  var haveLettersUpper = confirm("Do you want to include uppercase letters?");
+  var haveRandomNums = confirm("Do you want numbers?")
+  var haveSpecialChars = confirm("Do you want to include special characters?")
+
+
+  for (var i = 0; i < passLength; i++) {
+    var charType = Math.floor(Math.random() * 4); // For each password character, choose case 0, 1, 2, or 3
+    
     var randomChar = Math.floor(Math.random() * 10);
     password += randomChar;
     i++;
